218b246a5055278cb6d2f6c07f92cac6f97d680c
Ryan Moro <55772649+ryanmoro97@users.noreply.github.com>
2021-10-27 17:05:32 -0700

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 function openConn(){
     session_start();
     $db_user = "root";
-    $db_pw = "7r46QGmp";
+    $db_pw = "******";
     $server = "localhost";
     $db = "burnlearn";
     $conn = new mysqli($server, $db_user, $db_pw, $db);
